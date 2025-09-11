0a32670febe19e986525cf5dda6073470259878c
ptrckmgl <patrickmiguel.santiaguel@cvsu.edu.ph>
2025-09-11 15:04:51 +0800

    remove Material UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,9 @@
 // To run this type in CLI: npm run dev
 // Go to http://localhost:3000/
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world from React!
-    </h1>
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
