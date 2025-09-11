74458d06ce1be4a0aab35f727c7c74d478ba75d0
ptrckmgl <patrickmiguel.santiaguel@cvsu.edu.ph>
2025-09-11 17:01:32 +0800

    add shadcn/ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-// To run this type in CLI: npm run dev
-// Go to http://localhost:3000/
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
