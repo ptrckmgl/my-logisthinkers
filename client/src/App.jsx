528ac4e53c44072ce0f1a8d35b0cf3ec579f4982
ptrckmgl <patrickmiguel.santiaguel@cvsu.edu.ph>
2025-09-26 21:00:30 +0800

    test commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 // 1. enter in command: cd client
 // 2. enter in command: npm run dev
 // 3. ctrl + click the Local: http://localhost:5173/
+// 4. test
 import Dashboard from "./pages/dashboard";
 import LoginPage from "./pages/login";
 
